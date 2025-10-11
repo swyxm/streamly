d5b17141b9eef1f57b6dea68a56a699d0497b659
swyxm <swayamparekh1276@gmail.com>
2025-10-11 13:15:33 -0700

    rtmp service created

---
+++ apps/rtmp-server/index.js
@@ -0,0 +1,38 @@
+const NodeMediaServer = require("node-media-server");
+
+const httpConfig = {
+  port: 8000,
+  allow_origin: "*",
+  mediaroot: "./media",
+};
+
+const rtmpConfig = {
+  port: 1935,
+  chunk_size: 60000,
+  gop_cache: true,
+  ping: 10,
+  ping_timeout: 60,
+};
+
+const transformationConfig = {
+  ffmpeg: "./ffmpeg",
+  tasks: [
+    {
+      app: "live",
+      hls: true,
+      hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
+      hlsKeep: false,
+    },
+  ],
+  MediaRoot: "./media",
+};
+
+const config = {
+  http: httpConfig,
+  rtmp: rtmpConfig,
+  trans: transformationConfig,
+};
+
+const nms = new NodeMediaServer(config);
+
+nms.run();
